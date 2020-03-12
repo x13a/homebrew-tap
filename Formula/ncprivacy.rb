b45e6815179964098b7be150c89a618430fc8c1a
lucky <>
2020-03-13 02:00:08 +0300

    added ncprivacy formula clean readme

---
+++ Formula/ncprivacy.rb
@@ -0,0 +1,19 @@
+class Ncprivacy < Formula
+  include Language::Python::Virtualenv
+
+  desc "MacOS Notification Center Privacy"
+  homepage "https://bitbucket.org/x31a/ncprivacy"
+  url "https://bitbucket.org/x31a/ncprivacy/get/master.tar.gz"
+  version "0.0.6"
+  sha256 "70ee310d0a72b3d5c8c22b9fd94e4666fede261b838f85f60c46048590a7339b"
+
+  depends_on "python@3.8"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ncprivacy --version 2>&1")
+  end
+end
