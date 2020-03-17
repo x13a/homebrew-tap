cc7a253849a17f6b4dee4292258ed6048356041e
lucky <>
2020-03-17 23:21:39 +0300

    update morze formula

--- Formula/morze.rb
+++ Formula/morze.rb
@@ -1,9 +1,9 @@
 class Morze < Formula
   desc "Morse coder"
   homepage "https://bitbucket.org/x31a/morze"
-  url "https://bitbucket.org/x31a/morze/get/0.0.25.tar.gz"
-  version "0.0.25"
-  sha256 "da59933cadafa4fe45bb4c3140bc81a6fe20fac98b060e9e51f0b5207f2973a4"
+  url "https://bitbucket.org/x31a/morze/get/0.0.26.tar.gz"
+  version "0.0.26"
+  sha256 "0e8843f61bd547762635d7f5d9b90bc389d8ec222c1799a362f52f7da1c93de5"
 
   depends_on "rust" => :build
 
