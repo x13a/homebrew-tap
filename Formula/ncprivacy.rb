7dc45593da05c62d40e4f9c5f0f932f48bbe1e75
lucky <>
2020-03-25 18:46:57 +0300

    update ncprivacy formula

--- Formula/ncprivacy.rb
+++ Formula/ncprivacy.rb
@@ -3,9 +3,9 @@ class Ncprivacy < Formula
 
   desc "MacOS Notification Center Privacy"
   homepage "https://bitbucket.org/x31a/ncprivacy"
-  url "https://bitbucket.org/x31a/ncprivacy/get/0.0.6.tar.gz"
-  version "0.0.6"
-  sha256 "70ee310d0a72b3d5c8c22b9fd94e4666fede261b838f85f60c46048590a7339b"
+  url "https://bitbucket.org/x31a/ncprivacy/get/0.0.7.tar.gz"
+  version "0.0.7"
+  sha256 "e60e8baca15306c38f033ad04fac0e5042bc8663c9100fff86aaef4b0e1064d5"
 
   depends_on "python@3.8"
 
