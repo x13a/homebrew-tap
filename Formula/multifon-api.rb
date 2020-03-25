5f76bd6a71b7cbd0c6aa9ef3c7f12a42b5af21a6
lucky <>
2020-03-25 18:25:07 +0300

    update multifon-api formula

--- Formula/multifon-api.rb
+++ Formula/multifon-api.rb
@@ -1,14 +1,14 @@
 class MultifonApi < Formula
   desc "Multifon API"
   homepage "https://bitbucket.org/x31a/multifon-api"
-  url "https://bitbucket.org/x31a/multifon-api/get/0.0.14.tar.gz"
-  version "0.0.14"
-  sha256 "a2fb6e3ea9eedfee49a9b4d2e5088812433a75e47cf4043a03ba5b00aae8d6a4"
+  url "https://bitbucket.org/x31a/multifon-api/get/0.0.15.tar.gz"
+  version "0.0.15"
+  sha256 "dc98912b23c18b78f49c4367788c0af9d211a521f3797c2f2b937e600bba8a42"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"multifon-api", "./multifon_api"
+    system "go", "build", "-o", bin/"multifon-api", "./src"
   end
 
   test do
