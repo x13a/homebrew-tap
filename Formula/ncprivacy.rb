81e999c5f2db7d294a6bcdb460e3b9f94e5893b4
lucky <>
2020-04-20 22:26:28 +0300

    update ncprivacy formula

--- Formula/ncprivacy.rb
+++ Formula/ncprivacy.rb
@@ -5,8 +5,8 @@ class Ncprivacy < Formula
   homepage "https://bitbucket.org/x31a/ncprivacy"
   url "https://bitbucket.org/x31a/ncprivacy/get/0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "6262d67cee3caf5f1c129cf7b7ac1e14f55b3d53de4da03af284af1481884400"
-  revision 2
+  sha256 "dc4378ce0772cd46b8dc2da497c2c84274d7748ea4795685abbba8091021617a"
+  revision 3
 
   depends_on "python@3.8"
 
