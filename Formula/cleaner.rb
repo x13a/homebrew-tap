f5c72a1abdc888f9e76923d9afd72e5a5a9adf50
lucky <>
2020-04-23 00:59:52 +0300

    update cleaner formula

--- Formula/cleaner.rb
+++ Formula/cleaner.rb
@@ -1,9 +1,9 @@
 class Cleaner < Formula
   desc "Cleaner"
   homepage "https://bitbucket.org/x31a/cleaner"
-  url "https://bitbucket.org/x31a/cleaner/get/0.0.2.tar.gz"
-  version "0.0.2"
-  sha256 "71cf18a25d73dec5042094092b5ceeb3d5ee11d8a933bbc8d2bb86966be577d5"
+  url "https://bitbucket.org/x31a/cleaner/get/0.0.3.tar.gz"
+  version "0.0.3"
+  sha256 "0bded8bf104b61e4d7a3b81e654394128351aab34825765518bc3213814f53e2"
 
   depends_on "go" => :build
 
