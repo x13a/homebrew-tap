ee77b28b73159de01119ab10e8422e8ab927e23c
lucky <>
2020-04-24 01:31:14 +0300

    update ncprivacy formula

--- Formula/ncprivacy.rb
+++ Formula/ncprivacy.rb
@@ -2,11 +2,11 @@ class Ncprivacy < Formula
   include Language::Python::Virtualenv
 
   desc "MacOS Notification Center Privacy"
-  homepage "https://bitbucket.org/x31a/ncprivacy"
-  url "https://bitbucket.org/x31a/ncprivacy/get/0.1.0.tar.gz"
   version "0.1.0"
+  homepage "https://bitbucket.org/x31a/ncprivacy"
+  url "#{homepage}/get/#{version}.tar.gz"
   sha256 "dc4378ce0772cd46b8dc2da497c2c84274d7748ea4795685abbba8091021617a"
-  revision 3
+  revision 4
 
   depends_on "python@3.8"
 
@@ -15,6 +15,6 @@ class Ncprivacy < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/ncprivacy --version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/#{name} --version 2>&1")
   end
 end
