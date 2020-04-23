9fba208e5cb4c809c1b569c8068b9d595ecd6364
lucky <>
2020-04-24 01:25:26 +0300

    update cleaner formula

--- Formula/cleaner.rb
+++ Formula/cleaner.rb
@@ -1,17 +1,18 @@
 class Cleaner < Formula
-  desc "Cleaner"
-  homepage "https://bitbucket.org/x31a/cleaner"
-  url "https://bitbucket.org/x31a/cleaner/get/0.0.4.tar.gz"
+  desc ""
   version "0.0.4"
+  homepage "https://bitbucket.org/x31a/cleaner"
+  url "#{homepage}/get/#{version}.tar.gz"
   sha256 "1daf1a555703debd45eed63c7fa4cbb0219d5ca626b7438c502793731fca0e99"
+  revision 1
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"cleaner", "./src"
+    system "go", "build", "-o", bin/"#{name}", "./src"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/cleaner -V 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/#{name} -V 2>&1")
   end
 end
