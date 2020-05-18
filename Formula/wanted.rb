5a688dafed848d62de65e8de869cf0ddfb3e1cb1
lucky <>
2020-05-18 18:33:43 +0300

    update wanted formula

--- Formula/wanted.rb
+++ Formula/wanted.rb
@@ -1,6 +1,6 @@
 class Wanted < Formula
-  version "0.0.9"
-  sha256 "281f35fba067b967068b7c1095af77a078ee018265a6b0a7d499a4ae513f6c83"
+  version "0.0.10"
+  sha256 "85ed678019973cdd2c8c7471e7755adfe80d0b2998ee943e8e02c9b137912ecc"
 
   desc "Run predefined tasks on signal receive"
   homepage "https://bitbucket.org/x31a/wanted"
