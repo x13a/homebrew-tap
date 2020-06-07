04e1bec6588a011036b39ca3d190a9c5a726dd75
lucky <>
2020-06-07 22:32:32 +0300

    update multifon-api formula

--- Formula/multifon-api.rb
+++ Formula/multifon-api.rb
@@ -1,7 +1,7 @@
 class MultifonApi < Formula
   version "0.2.0"
   sha256 "c3568d3c08df89cb27eaa48f7a702a067dbc93000508ba9f38a7edca585eaad5"
-  revision 3
+  revision 4
 
   desc "Multifon API"
   homepage "https://bitbucket.org/x31a/multifon-api"
@@ -12,6 +12,13 @@ class MultifonApi < Formula
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
+    prefix.install "config/"
+    chmod 0600, prefix/"config/multifon.json"
+  end
+
+  def caveats; <<~EOS
+    Config sample filepath is #{prefix}/config/multifon.json
+  EOS
   end
 
   test do
