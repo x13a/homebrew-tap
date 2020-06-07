772dea91a97cf6b5137263fbbad10eb01470b3b8
lucky <>
2020-06-07 22:05:13 +0300

    update wanted formula

--- Formula/wanted.rb
+++ Formula/wanted.rb
@@ -1,6 +1,7 @@
 class Wanted < Formula
   version "0.1.4"
   sha256 "7dbdf3aac6aa73e0b7c0c9e98106ee5e4a2a9be2992eae0286dacc5d24cb4d00"
+  revision 1
 
   desc "Run predefined tasks on signal or broadcast receive"
   homepage "https://bitbucket.org/x31a/wanted"
@@ -9,10 +10,8 @@ class Wanted < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"#{name}", "./src/"
-    etc.mkpath
-    etc.install "./config/#{name}.json"
-    chmod 0600, etc/"#{name}.json"
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
