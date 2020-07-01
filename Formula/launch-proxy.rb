e82535acde5581553f7dcdbc9c983c6dda13f88c
lucky <>
2020-07-01 21:30:24 +0300

    update launch-proxy formula

--- Formula/launch-proxy.rb
+++ Formula/launch-proxy.rb
@@ -1,6 +1,6 @@
 class LaunchProxy < Formula
-  version "0.1.0"
-  sha256 "902188b0159748e848ac035b042b1be8dc2c31111b921a63e819427cb357b41b"
+  version "0.1.1"
+  sha256 "fdc9a9d114417d8b17010e7ff7fd9f3fc2b7e1ead2fa6621448de4a834a7a9bc"
 
   desc "Launch daemon sockets proxy for macOS"
   homepage "https://bitbucket.org/x31a/launch-proxy"
@@ -11,10 +11,72 @@ class LaunchProxy < Formula
   def install
     system "make"
     sbin.install "./target/#{name}"
-    pkgshare.install "./plist/me.lucky.#{name}.plist"
+  end
+
+  plist_options :startup => true
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <true/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_sbin}/#{name}</string>
+          <string>-d</string>
+          <string>:5350</string>
+          <string>-p</string>
+          <string>/Library/LaunchDaemons/#{plist_name}</string>
+        </array>
+        <key>UserName</key>
+        <string>nobody</string>
+        <key>GroupName</key>
+        <string>nogroup</string>
+        <key>StandardOutPath</key>
+        <string>/dev/null</string>
+        <key>StandardErrorPath</key>
+        <string>/dev/null</string>
+        <key>Sockets</key>
+        <dict>
+          <key>tcp4</key>
+          <dict>
+            <key>SockNodeName</key>
+            <string>127.0.0.1</string>
+            <key>SockServiceName</key>
+            <string>domain</string>
+            <key>SockType</key>
+            <string>stream</string>
+            <key>SockFamily</key>
+            <string>IPv4</string>
+            <key>SockProtocol</key>
+            <string>TCP</string>
+          </dict>
+          <key>udp4</key>
+          <dict>
+            <key>SockNodeName</key>
+            <string>127.0.0.1</string>
+            <key>SockServiceName</key>
+            <string>domain</string>
+            <key>SockType</key>
+            <string>dgram</string>
+            <key>SockFamily</key>
+            <string>IPv4</string>
+            <key>SockProtocol</key>
+            <string>UDP</string>
+          </dict>
+        </dict>
+      </dict>
+    </plist>
+  EOS
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/#{name} -V 2>&1")
+    assert_match version.to_s, shell_output("#{sbin}/#{name} -V 2>&1")
   end
 end
