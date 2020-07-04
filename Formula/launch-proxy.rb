c30de226649a778bc62a80e0bea36a32c92baa63
lucky <>
2020-07-04 17:15:08 +0300

    update launch-proxy formula

--- Formula/launch-proxy.rb
+++ Formula/launch-proxy.rb
@@ -1,6 +1,6 @@
 class LaunchProxy < Formula
-  version "0.1.2"
-  sha256 "5fad6c8f1e49d354708b6998920c1d1f125c07d15a345002313bf7b783ea9d4a"
+  version "0.1.3"
+  sha256 "b080a0987ae2415679c3475f32aeece8e3352df5fcc3cacee672e7decf4ba7f4"
 
   desc "Launch daemon sockets proxy for macOS"
   homepage "https://bitbucket.org/x31a/launch-proxy"
