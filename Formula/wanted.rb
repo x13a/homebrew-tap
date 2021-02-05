9b04c54e6fdd561ec07946b89aefca48981408fc
lucky <>
2021-02-05 12:31:03 +0300

    update wanted formula

--- Formula/wanted.rb
+++ Formula/wanted.rb
@@ -1,11 +1,11 @@
 class Wanted < Formula
   version "0.1.6"
-  sha256 "e58af3543d90b05a30bf48ed75f9011e5aa399d8b2f30a20a9c8930b674d2263"
-  revision 1
+  sha256 "45365145162a7fccb32f4b6e5fbebab06e83d7dfee6dda8cb9429407351b0be9"
+  revision 2
 
   desc "Run predefined tasks on signal or broadcast receive"
-  homepage "https://bitbucket.org/x31a/wanted"
-  url "#{homepage}/get/#{version}.tar.gz"
+  homepage "https://github.com/x13a/wanted"
+  url "#{homepage}/archive/#{version}.tar.gz"
 
   depends_on "go" => :build
 
