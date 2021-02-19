bb22d9bb4d0bdfcb643f8cb1ebf200cdcdf03617
lucky <>
2021-02-19 23:17:47 +0300

    update balong-v7r11 formula

--- Formula/balong-v7r11.rb
+++ Formula/balong-v7r11.rb
@@ -1,6 +1,7 @@
 class BalongV7r11 < Formula
     version "0.1.0"
     sha256 "c8f18bf07f61e4d18da65e117bea73a23b94506a9b1dd3664c0a4cb45d58c0c1"
+    revision 1
     
     desc "Bruteforce OEM and NCK codes for Balong V7R11"
     homepage "https://github.com/x13a/balong-v7r11"
@@ -17,4 +18,3 @@ class BalongV7r11 < Formula
       assert_match version.to_s, shell_output("#{opt}/#{name} -V 2>&1")
     end
   end
-    
