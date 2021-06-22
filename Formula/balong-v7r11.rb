e608dfafdefd5e335f270f0d3cf35d24d7a51af7
lucky <>
2021-06-22 23:24:37 +0300

    update balong-v7r11 formula

--- Formula/balong-v7r11.rb
+++ Formula/balong-v7r11.rb
@@ -1,13 +1,13 @@
 class BalongV7r11 < Formula
     version "0.1.0"
-    sha256 "c8f18bf07f61e4d18da65e117bea73a23b94506a9b1dd3664c0a4cb45d58c0c1"
-    revision 1
+    sha256 "716f126a637c95a68636e2ce8499fb11453939dcb24b5376032af72f475af434"
+    revision 2
     
     desc "Bruteforce OEM and NCK codes for Balong V7R11"
     homepage "https://github.com/x13a/balong-v7r11"
     url "#{homepage}/archive/#{version}.tar.gz"
     
-    depends_on "rust" => :build
+    depends_on "zig" => :build
     
     def install
       system "make"
