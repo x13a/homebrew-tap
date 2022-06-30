0efcaada13c1170d6d0f7a92dd4a1db14baef0d4
lucky <lucky@lucky-book.local>
2022-06-30 19:07:13 +0300

    fix pschk

--- Formula/pschk.rb
+++ Formula/pschk.rb
@@ -1,13 +1,13 @@
 class Pschk < Formula
   version "0.1.7"
-  sha256 "5be871f678e4b4906757be3303469ef689bc19c8d4572147a17108a0c9342622"
+  sha256 "449e159b79f7e98e140955c3cd762b133800401f329fbb9b2c9551feb1b80d1e"
 
   desc "Check for suspicious processes on macOS"
   homepage "https://github.com/x13a/pschk"
   url "#{homepage}/archive/#{version}.tar.gz"
 
   def install
-    system "make"
+    system "swift", "build", "-c", "release", "--disable-sandbox"
     system "make", "install", "prefix=#{prefix}"
   end
 
