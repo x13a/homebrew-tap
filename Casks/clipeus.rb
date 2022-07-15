848619c79b6b2f3625e789cc646b73b1b4d7cffb
lucky <lucky@lucky-book.local>
2022-07-15 16:13:50 +0300

    fix clipeus

--- Casks/clipeus.rb
+++ Casks/clipeus.rb
@@ -1,6 +1,6 @@
 cask "clipeus" do
   version "1.0.0"
-  sha256 "58d89a05899358b4012aedcd38261e601bc05eda186a09d0ca09628c35ee94ae"
+  sha256 "f32a57434223f10699b9a9965b8b21631645de2f1facbaaa749d6ffe6a202776"
 
   homepage "https://github.com/x13a/ClipeusX"
   url "#{homepage}/releases/download/v#{version}/Clipeus_#{version}.zip"
