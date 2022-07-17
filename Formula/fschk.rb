25856fb7d318099a29434c724c7eac1cd770ecc5
lucky <lucky@lucky-book.local>
2022-07-17 20:35:31 +0300

    update fschk

--- Formula/fschk.rb
+++ Formula/fschk.rb
@@ -1,6 +1,7 @@
 class Fschk < Formula
   version "0.1.5"
   sha256 "9d7bb97516f8f193137188a2d2ee9ea62262c1f3ac4e905727492b3adef94526"
+  version 1
 
   desc "List persistence items on macOS"
   homepage "https://github.com/x13a/fschk"
